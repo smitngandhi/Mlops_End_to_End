15d5c370da98d3901742b968dc2de40028720e3c
Smit Gandhi <smitgandhi585@gmail.com>
2025-05-27 07:54:05 +0530

    Experimenting with flask and Docker

---
+++ Flask/flask/temp.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, url_for, request , render_template
+app = Flask(__name__)
+
+
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      return render_template("temp.html")
+
+if __name__ == '__main__':
+   app.run(debug = True)
